37e34e2127c26e5390c9bac347fd00ff884f4401
Tanaka Arata <ky.37f.9850@s.thers.ac.jp>
2024-12-27 14:28:06 +0900

    fix

--- gen1_train_frame.py
+++ gen1_train_frame.py
@@ -18,7 +18,7 @@ gpu_ids_str = ",".join(map(str, gpu_ids))
 for dt in event_frame_dts:
     command = f"""
     python3 train.py model=rnndet dataset=gen1 dataset.path={data_dir} wandb.project_name=RVT_gen1_frame_{dt} \
-    wandb.group_name=gen1 +experiment/gen1={mdl_cfg}.yaml hardware.gpus={gpu_ids_str} \
+    wandb.group_name=gen1 +experiment/gen1={mdl_cfg}.yaml hardware.gpus="[ {gpu_ids_str} ]" \
     batch_size.train={batch_size_per_gpu} batch_size.eval={batch_size_per_gpu} \
     hardware.num_workers.train={train_workers_per_gpu} hardware.num_workers.eval={eval_workers_per_gpu} \
     dataset.ev_repr_name="'event_frame_dt={dt}'" model.backbone.input_channels={input_channels}

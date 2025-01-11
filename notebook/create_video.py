f9c4edfad148b240b4f6e458a19b15c77b67c6bd
Arata Tanaka <ky.37f.9850@s.thers.ac.jp>
2025-01-11 13:52:16 +0900

    bit fix

--- notebook/create_video.py
+++ notebook/create_video.py
@@ -13,15 +13,15 @@ def main(yaml_path: str, output_path: str, fps: int=10, max_sequence_length: int
     config = OmegaConf.load(yaml_path)
     dynamically_modify_train_config(config)
     video_writer.run(config, max_sequence_length)
-    video_writer.release()
+    video_writer.video_writer.release()
 
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
-    parser.add_argument('y','--yaml_path', type=str, required=True)
-    parser.add_argument('o','--output_path', type=str, required=True)
-    parser.add_argument('f','--fps', type=int, default=10)
-    parser.add_argument('m','--max_sequence_length', type=int, default=1)
+    parser.add_argument('-y','--yaml_path', type=str, required=True)
+    parser.add_argument('-o','--output_path', type=str, required=True)
+    parser.add_argument('-f','--fps', type=int, default=10)
+    parser.add_argument('-m','--max_sequence_length', type=int, default=1)
 
     args = parser.parse_args()
     main(args.yaml_path, args.output_path, args.fps, args.max_sequence_length)
